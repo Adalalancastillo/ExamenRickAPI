405a514ef788507e263071382f2263698f15ff5a
AdalCastillo <adalalancastillo@gmail.com>
2023-10-06 19:20:32 -0700

    Examen

---
+++ src/components/RickApi.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+
+function RickApi() {
+
+    const [datos, setDatos] = useState([])
+
+    const RickApiList = async() => {
+        const data = await fetch('https://rickandmortyapi.com/api/character')
+        const datos = await data.json()
+        setDatos(datos.results)
+        console.log(datos.results)
+    };
+
+    useEffect(() => {
+      RickApiList()
+    }, [])
+
+  return (
+    <>
+      <div>
+        <h1>RICK API</h1>
+        <h2>PERSONAJES</h2>
+        {datos.map(data => (
+            <div key={data.id}>
+              <img src={data.image} />
+              <h4>{data.name}</h4>
+              <p>{data.species}</p>
+              <p>{data.gender}</p>
+            </div>
+        ))}
+      </div>
+    </>
+  )
+}
+
+export default RickApi
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import RickApi from './components/RickApi.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RickApi />
+  </React.StrictMode>,
+)
